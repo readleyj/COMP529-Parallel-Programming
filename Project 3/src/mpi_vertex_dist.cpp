c4149422d1476b7dc40d9b527052ab352c14a1c4
Ismayil Ismayilov <iismayilov4574@gmail.com>
2022-01-03 18:19:43 +0300

    Added dynamic send receive. Not working

--- Project 3/src/mpi_vertex_dist.cpp
+++ Project 3/src/mpi_vertex_dist.cpp
@@ -3,8 +3,31 @@ int mpi_vertex_dist(graph_t *graph, int start_vertex, int *result)
     int global_num_vertices = graph->num_vertices;
 
     int local_begin_vertex = (global_num_vertices * my_rank) / num_proc;
-    int local_num_vertices = (global_num_vertices * (my_rank + 1)) / num_proc - (global_num_vertices * my_rank) / num_proc;
+    int local_num_vertices = global_num_vertices / num_proc;
     int local_end_vertex = local_begin_vertex + local_num_vertices;
+
+    if (my_rank == num_proc - 1)
+    {
+        local_end_vertex = global_num_vertices;
+        local_num_vertices = global_num_vertices - local_begin_vertex;
+    }
+
+    auto find_owner = [&global_num_vertices](int vertex) -> int
+    {
+        int local_num_vertices = global_num_vertices / num_proc;
+
+        int proc = vertex / local_num_vertices;
+
+        if (proc >= num_proc)
+        {
+            proc--;
+        }
+
+        return proc;
+    };
+
+    // printf("%d %d %d %d\n", local_begin_vertex, local_end_vertex, my_rank, global_num_vertices);
+    // printf("%d\n", find_owner(253435));
 
     fill_n(result, global_num_vertices, MAX_DIST);
 
@@ -17,6 +40,8 @@ int mpi_vertex_dist(graph_t *graph, int start_vertex, int *result)
     int global_should_run = true;
 
     result[start_vertex] = local_depth;
+
+    vector<pair<int, int>> owners[num_proc];
 
     while (local_should_run || global_should_run)
     {
@@ -33,11 +58,58 @@ int mpi_vertex_dist(graph_t *graph, int start_vertex, int *result)
                     if (result[neighbor] > local_depth + 1)
                     {
                         result[neighbor] = local_depth + 1;
-                        local_should_run = true;
+
+                        auto neighbor_owner = find_owner(neighbor);
+
+                        if (neighbor_owner != my_rank)
+                        {
+                            owners[neighbor_owner].push_back({neighbor, local_depth + 1});
+                        }
                     }
                 }
             }
         }
+
+        for (int i = 0; i < num_proc; i++)
+        {
+            if (i == my_rank)
+            {
+                continue;
+            }
+
+            auto owner = owners[i];
+
+            MPI_Send((void *)owner.data(), sizeof(pair<int, int>) * owner.size(), MPI_BYTE, i, 0, MPI_COMM_WORLD);
+        }
+
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        for (int i = 0; i < num_proc; i++)
+        {
+            if (i == my_rank)
+            {
+                continue;
+            }
+
+            MPI_Status status;
+            MPI_Probe(i, 0, MPI_COMM_WORLD, &status);
+
+            int number_amount;
+            MPI_Get_count(&status, MPI_BYTE, &number_amount);
+
+            vector<pair<int, int>> recv_buf;
+
+            recv_buf.resize(number_amount / sizeof(pair<int, int>));
+
+            MPI_Recv((void *)recv_buf.data(), number_amount, MPI_BYTE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            for (auto what : recv_buf)
+            {
+                cout << "Sup bitch " << what.first << ' ' << what.second << endl;
+            }
+        }
+
+        MPI_Barrier(MPI_COMM_WORLD);
 
         local_depth++;
 
